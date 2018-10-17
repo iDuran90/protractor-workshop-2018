f7b64de715e75b03619c1eaf5eba8b61c338bf7c
Jorge Durán <jdp0990@gmail.com>
2018-10-17 12:00:06 -0500

    improve tests (#2)

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,8 +1,12 @@
-import { Config } from 'protractor';
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub'
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
 };
-
