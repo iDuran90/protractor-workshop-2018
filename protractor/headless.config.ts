5db995a05497b010d8499d9f13758113c6e4f527
Jorge Durán Páez <jorge.duran@mulesoft.com>
2018-10-17 12:47:03 -0500

    add chrome headless configuration

---
+++ protractor/headless.config.ts
@@ -0,0 +1,18 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
