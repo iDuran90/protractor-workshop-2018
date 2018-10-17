b7dad7672fea0acc25b6382ef950005a3d77bc73
Jorge Durán Páez <jorge.duran@mulesoft.com>
2018-10-17 11:58:10 -0500

    improve console reports for tests executions

---
+++ protractor/helpers/reporter.ts
@@ -0,0 +1,8 @@
+import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter';
+
+export let reporter = () => {
+  jasmine.getEnv().addReporter(new SpecReporter({
+    customProcessors: [DisplayProcessor]
+  }));
+};
+
