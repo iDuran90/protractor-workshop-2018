19ee2f7bebd1e1f9f3477ff79ab2f0cc42ef7d05
Jorge Durán <jdp0990@gmail.com>
2018-10-18 10:24:57 -0500

    add test using css selectors (#9)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,7 +3,8 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/spec.js' ],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
