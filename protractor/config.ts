9b542f8370dff786428e8d0993f4b30e18aa4eb1
Jorge Durán Páez <jorge.duran@mulesoft.com>
2018-10-18 09:39:27 -0500

    add test using css selectors

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,10 +3,17 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/spec.js' ],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--window-size=1024,768']
+    }
   }
 };
