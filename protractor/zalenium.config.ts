5353f913d42d73ff0a18a385aa9dcee75b61068c
Jorge Durán Páez <jorge.duran@mulesoft.com>
2018-10-30 10:18:55 -0500

    configure zalenium

---
+++ protractor/zalenium.config.ts
@@ -0,0 +1,37 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 30000,
+  SELENIUM_PROMISE_MANAGER: false,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    browser.driver.manage().window().setSize(800, 600);
+    browser.manage().timeouts().implicitlyWait(0);
+    reporter();
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  multiCapabilities
+};
